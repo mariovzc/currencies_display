6222791f64c2e974715babfb561be0cfd683f9a4
¨Mario <mario.vizcainos@gmail.com>
2017-10-07 23:47:03 -0500

    main card basic style

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,27 @@
 <template>
-  
+  <v-app id="e3" standalone>
+    <main>
+      <v-layout row wrap>
+        <v-flex 
+          xs8
+          lg8
+          offset-xs2
+        >
+          <v-card class="blue-grey darken-2 white--text">
+            <v-card-title primary-title>
+              <div class="headline">Unlimited music now</div>
+              <div>Listen to your favorite artists and albums whenenver and wherever, online and offline.</div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn flat dark>Listen now</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </main>
+  </v-app>
 </template>
+
 <script>
 export default {
   name: 'Main'
